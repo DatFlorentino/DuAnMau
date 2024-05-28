6ce309225097efa6e074c1b756b044405dbed5d5
Đức Danh <73422801+doremoniip@users.noreply.github.com>
2024-05-28 12:00:53 +0700

    PlayerMovement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 moveInput;
+    Rigidbody2D rig;
+    [SerializeField] float speed = 10f;
+    [SerializeField] float jumpspeed = 5f;
+    CapsuleCollider2D col;
+    float startgravityscale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rig = GetComponent<Rigidbody2D>();
+        col = GetComponent<CapsuleCollider2D>();
+        startgravityscale = rig.gravityScale;
+    }
+        // Update is called once per frame
+        void Update()
+    {
+        Run();
+        Flip();
+    }
+
+    void Run()
+    {
+        rig.velocity = new Vector2(moveInput.x * speed, rig.velocity.y);
+        bool havemove = Mathf.Abs(rig.velocity.x) > Mathf.Epsilon;
+    }
+
+    void Flip()
+    {
+        bool havemove = Mathf.Abs(rig.velocity.x) > Mathf.Epsilon;
+
+        if (havemove)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rig.velocity.x), 1f);
+        }
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            rig.velocity += new Vector2(0f, jumpspeed);
+        }
+    }
+
+
+}
