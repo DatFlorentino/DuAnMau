85dfb5065086e4fba68ad4a5ff50da11c687155f
Đức Danh <73422801+doremoniip@users.noreply.github.com>
2024-05-31 23:07:11 +0700

    UpdataMap2

---
+++ StoreHelper.cs
@@ -0,0 +1,35 @@
+﻿
+namespace DefaultNamespace
+{
+    using DefaultNameSpace;
+    using System.Collections.Generic;
+    using System.IO;
+    using UnityEngine;
+    using UnityEngine.Playables;
+
+    public class StorageHelper
+    {
+        private readonly string filename = "game_data.txt";
+        public GameDataPlayed played;
+        public void LoadData()
+        {
+            played = new GameDataPlayed();
+            {
+                plays = new List<GameData>();
+            };
+            string dataAsJson = StorageManager.LoadFromFile(filename);
+            if (dataAsJson != null)
+            {
+                // chuyen chuoi json thanh object
+                played = JsonUtility.FromJson<GameDataPlayed>(dataAsJson);
+            }
+        }
+        public void SaveData()
+        {
+            //chuyen object thanh chuoi json
+            string dataAsJson = JsonUtility.ToJson(played);
+            //LUU chuoi json vao file
+            StorageManager.SaveToFile(filename, dataAsJson);
+        }
+    }
+}
