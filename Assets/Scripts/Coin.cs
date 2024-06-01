71f2a967c1e5f5d24e983c28ffa539113602b3de
DatFlorentino <nai.hoalan@gmail.com>
2024-06-01 19:49:31 +0700

    Add Nhiệm Vụ nhặt 20 coin

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,20 +1,22 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
     [SerializeField] AudioClip CoinPickup;
-    [SerializeField] float CoinValue = 50;
+    [SerializeField] int CoinValue = 50; // Giá trị điểm của mỗi coin
 
     private bool isCollected = false;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && !isCollected)
         {
             isCollected = true;
-            //tang diem
-            FindObjectOfType<GameController>().AddScore((int)CoinValue);
+            // Cộng điểm và tính số lần nhặt coin
+            GameController gameController = FindObjectOfType<GameController>();
+            gameController.CollectCoin(CoinValue);
             AudioSource.PlayClipAtPoint(CoinPickup, Camera.main.transform.position);
             gameObject.SetActive(false);
             Destroy(gameObject);
